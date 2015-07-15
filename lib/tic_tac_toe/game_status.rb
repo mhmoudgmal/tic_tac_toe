cff9d315ee7adebb209931af23c7fdcb655051b1
Mahmoud Gamal <mhmoudgmal.89@gmail.com>
2015-07-15 04:46:49 +0200

    check against win status every time current player make change to the board

--- lib/tic_tac_toe/game_status.rb
+++ lib/tic_tac_toe/game_status.rb
@@ -2,10 +2,21 @@ module TicTacToe
   ##
   # GameStatus module, included in the GameBoard to provide game status stuff
   module GameStatus
-    @status = nil
+    STATUSES = [:win, :block]
 
     def self.included(klass)
       attr_reader :status
+      
+      klass.instance_variable_set(:@status, nil)
+
+      # GameBoard possible win cells commbined
+      klass.const_set(:POSSIBLE_WIN_CELLS,
+        [ [1, 2, 3], [1, 4, 7], [1, 5, 9],
+          [2, 5, 8],
+          [3, 5, 7], [3, 6, 9],
+          [4, 5, 6],
+          [7, 8, 9] ]
+      )
     end
 
 
@@ -14,8 +25,13 @@ module TicTacToe
     # [Returns]
     # A Boolean representing whether the GameBoard#current_player won the game
     def win?
-      # FIXME: dynamically check after each player turn
-      return false
+      GameBoard::POSSIBLE_WIN_CELLS.each do |possible_win|
+        if possible_win.map{|p| p - 1}.all? {|cell| board[cell] == @current_player.tic_symbol}
+          return true
+        end
+      end
+
+      false
     end
 
     # Checks for game over
