0c17dbf71b05a980daf3fa349eba0a752d1ed224
Mahmoud Gamal <mhmoudgmal.89@gmail.com>
2016-02-03 13:00:53 +0100

    Continue game

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,11 +1,15 @@
 module GamesHelper
-
   def play_or_watch game
-    game['players'].include?(nil) ? 'Play' : 'Watch'
+    if game['players'].include?(nil)
+      'Play'
+    elsif game['players'].include?(current_user)
+      'Continue'
+    else
+      'Watch'
+    end
   end
 
   def player_turn
     "#{@game['current_player'] == current_user ? 'Your' : @game['current_player']}"
   end
-
 end
